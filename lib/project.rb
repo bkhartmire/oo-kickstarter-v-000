62c7e25b604d1bac036ecc11c5adfa3b544f5e3c
Brittany Hartmire <bkhartmire@gmail.com>
2017-11-23 20:46:12 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -7,7 +7,7 @@ class Project
 
   def add_backer(backer)
     self.backers << backer
-    if backer.back_project.none?(self)
+    if (backer.back_project.none?(self))
       backer.back_project(self)
     else
       backer
